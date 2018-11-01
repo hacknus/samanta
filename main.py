85135de2f75ae0fa88a69691e71c24a535ebf84e
Linus <linus@linus-macbook-pro.home>
2018-11-02 00:46:24 +0100

    animation added

--- main.py
+++ main.py
@@ -13,21 +13,23 @@ class ant:
 		self.position = np.array(xy)
 		self.alive = True
 		self.tabu = []
-		self.last_path = None
+		self.last_path = [np.array([np.nan,np.nan]),np.array([np.nan,np.nan])]
 
 	def make_move(self,feromone,waypoints):
 		''' 
 		takes feromone and a list of all distances to waypoints as arguments
 		and then calls probability and decision function in order to make a move
 		'''
-		d = self.calc_dist(waypoints)
-		self.tabu.append(self.position)
-		probabilities = self.prob(feromone,d)
-		dec = self.decision(probabilities)
-		if not self.alive:
+		if not self.alive:						#check if alive
 			return False
-		self.position = dec
-		self.last_path = [ self.tabu[-1], self.position ]
+		d = self.calc_dist(waypoints)			#get distance dictionary
+		self.tabu.append(self.position)			#append current position to tabu list
+		probabilities = self.prob(feromone,d)	#get probabilities of each path (to waypoints)
+		p = self.decision(probabilities)		#get decision
+		if not self.alive:						#check if alive (decision() could have changed this state)
+			return False
+		self.position = p.position 	#make move (update position)
+		self.last_path = [ self.tabu[-1], self.position ]	#set last path
 		return True
 
 	def calc_dist(self,waypoints):
@@ -62,7 +64,7 @@ class ant:
 	def prob(self,feromone,d):
 		''' takes feromone strength and distances (list) to next point as arguments 
 			returns the probabilities (list) of choosing these paths'''
-		p = [1]
+		p = [list(d.keys())[0]]
 		return p
 
 	def kill_ant(self):
@@ -83,7 +85,7 @@ def delta(k,p,i,j):
 	and returns value of 
 	delta tau
 	'''
-	if k.last_path == [ p[i],p[j] ] or k.last_path == [ p[j],p[i] ]:
+	if ( (k.last_path[0] == p[i].position).all() and (k.last_path[1] == p[j].position).all() ) or ( (k.last_path[0] == p[j].position).all() and (k.last_path[1] == p[i].position).all() ):
 		return Q/np.linalg.norm(k.last_path[0]-k.last_path[1])
 	else:
 		return 0
@@ -92,7 +94,7 @@ def delta(k,p,i,j):
 def update_feromone(f,ant_list,point_list):
 	''' takes feromone matrix, ant list and point list as arguments and returns the new feromone matrix'''
 	#linus (1. method ant quantity)
-	#could be vectorized
+	#could be vectorized and split in half (since tau is traceless and symmetrical)
 
 
 	f_new = np.copy(f)
@@ -100,22 +102,12 @@ def update_feromone(f,ant_list,point_list):
 		for j in range(len(f)):
 			s = 0
 			for k in ant_list:
+				if i==j:
+					continue
 				s += f[i][j]*rho + delta(k,point_list,i,j)
 			f_new[i][j] = s
 	return f_new
 
-
-
-def init():
-	#used to animate later on
-    line.set_ydata(np.ma.array(x, mask=True))
-    return line,
-
-
-def animate(i):
-	#used to animate later on
-    line.set_ydata(np.sin(x + i/10.0))  # update the data
-    return line,
 
 ######## initial
 #for nora (travelling salesman)
@@ -159,24 +151,53 @@ def draw_all_paths(p,feromone,paths=[]):
 	if paths:
 		for line in paths:
 			#clear all previously drawn paths (saves memory)
-			line.pop(0).remove()
+			try:
+				line.pop(0).remove()
+			except:
+				pass
 	for i in range(len(p)):
 		for j in range(len(p)):
 			if i!=j:
 				width = feromone[i][j]/10 #adjusts with according to feromone strength
+				print(width)
 				paths.append(ax.plot( [p[i][0], p[j][0]] ,[p[i][1], p[j][1]] ,color='blue',linewidth=width,zorder=-2))
-	return paths
+				#print([p[i][0], p[j][0]], [p[i][1], p[j][1]])
+				#paths[i][j].set_data( [p[i][0], p[j][0]], [p[i][1], p[j][1]] )
+	return paths,
 
+def run(t,feromone,paths):
+	feromone = update_feromone(feromone,ants,points)
+	paths = draw_all_paths(coordinates_points,feromone,paths)
+	for a in ants:
+		#let all ants make a move
+		if not a.make_move(feromone,points):
+			# if the ant is dead, add it to the list
+			dead_ants.append(a)
+	# after the loop above is finished we remove all dead ants from the ant list
+	# we have to do this after finishing the loop in order to avoid list out of range error!
+	for a in dead_ants:
+		ants.remove(a)
+	# if not ants:
+	# 	#if no more ants are alive, break
+	# 	return False
+	return
 
-
+def init():
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0,1)
+    return paths,
 
 
 if __name__ == '__main__':
 	fig, ax = plt.subplots()
-	#x = np.arange(0, 2*np.pi, 0.01)
-	#line, = ax.plot(x, np.sin(x))
-
+	xdata, ydata = [], []
 	points,ants,feromone = initial_condition()
+	paths = [ [ '' for i in range(len(points))] for j in range(len(points)) ]
+
+	for i in range(len(points)):
+		for j in range(len(points)):
+			ln, = ax.plot([], [], color='blue',linewidth=1,zorder=-2, animated=True)
+			paths[i][j] = ln
 	dead_ants = []
 
 	#get list of coordinates for plotting purposes
@@ -184,7 +205,7 @@ if __name__ == '__main__':
 	coordinates_ants = np.array([ [a.position[0],a.position[1]] for a in ants])
 
 
-	paths = draw_all_paths(coordinates_points,feromone)
+	#paths = draw_all_paths(coordinates_points,feromone,paths)
 	
 	ax.scatter(coordinates_points[:-2,0],coordinates_points[:-2,1],s=200,color='black',zorder=1)
 	ax.scatter(coordinates_points[-2:,0],coordinates_points[-2:,1],s=200,color='orange',zorder=1)	# points A and B
@@ -192,23 +213,6 @@ if __name__ == '__main__':
 
 
 	#begin main loop  ---- animation func is not implemented yet
-	t = 0
-	while t < 100:
-		feromone = update_feromone(feromone,ants,points)
-		paths = draw_all_paths(coordinates_points,feromone,paths)
-		for a in ants:
-			#let all ants make a move
-			if not a.make_move(feromone,points):
-				# if the ant is dead, add it to the list
-				dead_ants.append(a)
-		# after the loop above is finished we remove all dead ants from the ant list
-		# we have to do this after finishing the loop in order to avoid list out of range error!
-		for a in dead_ants:
-			ants.remove(a)
-		if not ants:
-			#if no more ants are alive, break
-			break
-		t += 1 
-		break
-	#ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), blit=False, interval=10,repeat=False, init_func=init)
+
+	ani = animation.FuncAnimation(fig, run, np.arange(1, 20), fargs=(feromone,paths),blit=False, interval=10,repeat=False, init_func=init)
 	plt.show()
