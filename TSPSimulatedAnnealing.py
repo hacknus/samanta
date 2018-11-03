c5f43b8d5577000eb546e37b42ca7d5722f3451b
norasalgo <nora.salgo@uzh.ch>
2018-11-03 18:28:38 +0100

    set up -change function and -probability function

--- TSPSimulatedAnnealing.py
+++ TSPSimulatedAnnealing.py
@@ -2,20 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class waypoint:
+class Waypoint:
     num = 0
     ''' class object of a waypoint, has attribute of position '''
 
     def __init__(self, xy):
         self.position = xy
         self.id = self.num + 1
-        waypoint.num = self.id
+        Waypoint.num = self.id
 
 
-def decision(probabilities):
-    # quinten
-    choice = 0
-    return choice
+
+def change_config1(config):
+    """
+randomly changes configuration: e.g.: a--b--c--d--e--f ==> a--e--d--c--b--f
+    arg: old config
+    return: new config
+    """
+    return config
+
+
+def change_config2(config):
+    """
+randomly inserts a part of the config somewhere else, e.g. a--b--c--d--e--f ==> a--d--e--b--c--f
+    :param config: old config
+    :return: new config
+    """
+    return config
+
+def cost_func(config):
+    """
+calculates cost resp. distance for the configuration
+    """
+    pass
+
+def acceptance_probability(cost_change, temp):
+    """
+Determines probability of accepting the new config
+    :param cost_change: relative change in cost between old config and new config
+    :param temp: current temperature
+    :return p: probability of acceptance
+    """
+    pass
+
+
+
 
 
 def main():
@@ -24,24 +55,22 @@ def main():
         break
 
 
-######## initial
+# ----------------- initial--------------
 # for nora (travelling salesman)
 def random_waypoints(n=10, set_seed=True):
     if set_seed:
         np.random.seed(0)  # to get each time the same random numbers
-    points = []
-    for i in range(n):
-        pos = np.random.rand(2)
-        points.append(waypoint(pos))
+    points = [Waypoint(np.random.rand(2)) for i in range(n)]
     return points
+
 
 def initial_condition(waypoints):
     return waypoints, np.random.shuffle(waypoints)
 
 
-########
+# --------------------
 
 
 if __name__ == '__main__':
-    points, config = initial_condition()
+    points, config = initial_condition(random_waypoints())
     main()
