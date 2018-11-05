863b7f0f75ec55c8c1d97acd65e10ef14e4567a5
norasalgo <nora.salgo@uzh.ch>
2018-11-05 15:45:23 +0100

    run funciton

--- TSPSimulatedAnnealing.py
+++ TSPSimulatedAnnealing.py
@@ -2,41 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Waypoint:
+class Cities:
     num = 0
     ''' class object of a waypoint, has attribute of position '''
 
     def __init__(self, xy):
         self.position = xy
         self.id = self.num + 1
-        Waypoint.num = self.id
+        Cities.num = self.id
 
 
-class Config:
-
-    def __init__(self,n):
-        self.n = n
-
-def temp_0(waypoints):
+def temp_0(dist):
     """
 estimates a good starting temperature
-    :param waypoints: used waypoints
+    :param dist: distance matrix for used cities
     :return start temperatur
     """
-    pass
+    random_config = [np.random.permutation(np.arange(10)) for i in range(100)]
+    cost_random_config = [cost(c, dist) for c in random_config]
+    t_0 = (max(cost_random_config) - min(cost_random_config)) * 1.1
+    return t_0
 
 
-def dist_array(waypoints):
+def dist_array(cities):
     """
-calculates distances between waypoints
-    :param waypoints: list of waypoints
-    :return dist_array: n x n array, with dist_array[i][j] = distance between ith and jth waypoint
+calculates distances between cities
+maybe np.linalg.norm
+    :param cities: list of cities
+    :return dist_array: n x n array, with dist_array[i][j] = distance between ith and jth city
     """
-    n = len(waypoints)
+    n = len(cities)
     dist = [
         [np.sqrt(
-            (waypoints[i].position[0] - waypoints[j].position[0]) ** 2 +
-            (waypoints[i].position[1] - waypoints[j].position[1]) ** 2
+            (cities[i].position[0] - cities[j].position[0]) ** 2 +
+            (cities[i].position[1] - cities[j].position[1]) ** 2
         ) for j in range(n)]
         for i in range(n)]
     return dist
@@ -56,7 +55,7 @@ Determines probability of accepting the new config
     :param config_k: config at time k
     :param config_k1: config at time k+1
     :param tempk: temp at time k
-    :param dist: distance array for used waypoints
+    :param dist: distance array for used cities
     :return p: probability of acceptance
     """
     delta = cost(config_k1, dist) - cost(config_k, dist)
@@ -65,6 +64,20 @@ Determines probability of accepting the new config
     else:
         p = np.exp(-delta / tempk)
     return p
+
+
+def swap2cities(config):
+    """
+swaps two cities in configuration
+    :param config: old configuration
+    :return: new configuration
+    """
+    i, j = np.random.randint(len(config), size=2)
+    new_config = np.copy(config)
+    temp = new_config[i]
+    new_config[i] = new_config[j]
+    new_config[j] = temp
+    return new_config
 
 
 def change_config1(config):
@@ -84,6 +97,7 @@ randomly inserts a part of the config somewhere else, e.g. a--b--c--d--e--f ==> 
     :return: new config
     """
     return config
+
 
 def move(p):
     """
@@ -108,7 +122,7 @@ def main():
 def random_waypoints(n=10, set_seed=True):
     if set_seed:
         np.random.seed(0)  # to get each time the same random numbers
-    points = [Waypoint(np.random.rand(2)) for i in range(n)]
+    points = [Cities(np.random.rand(2)) for i in range(n)]
     points_dict = {}
     for p in points:
         points_dict[p.id] = p
@@ -116,10 +130,42 @@ def random_waypoints(n=10, set_seed=True):
 
 
 def initial_condition(waypoints):
+    """
+1)choose random state, define T0, beta
+
+    :param waypoints:
+    :return:
+    """
     return waypoints, [i for i in range(len(waypoints))]
 
 
 # --------------------
+
+def run(config,t_k,dist,beta):
+    """
+2)create new state s'
+3)compute delta
+    decide if move to new stat
+        if yes: compute t_k+1 = beta* t_k
+4)repeat step 2, 3, keeping track of best solution until stoping cond
+    :param config: current configuration
+    :param t_k: current temperatur
+    :param dist: distance matrix for used cities
+    :param beta: cooling factor
+
+    """
+    config_accept = False
+    while not config_accept:
+        new_config = swap2cities(config)
+        p = acceptance_probability(config,new_config,t_k,dist)
+        if move(p):
+            t_k1 = t_k*beta
+            config_accept = True
+
+    return new_config, t_k1
+
+
+
 
 
 if __name__ == '__main__':
