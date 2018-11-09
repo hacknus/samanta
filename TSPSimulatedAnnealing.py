d920417da2c9c52f0b13cd8761d17de23f62c6b6
norasalgo <nora.salgo@uzh.ch>
2018-11-09 13:23:09 +0100

    blabla - still a lot to do

--- TSPSimulatedAnnealing.py
+++ TSPSimulatedAnnealing.py
@@ -8,15 +8,18 @@ class Cities:
 
     def __init__(self, xy):
         self.position = xy
-        self.id = self.num + 1
-        Cities.num = self.id
+        self.id = Cities.num
+        Cities.num += 1
+
+    def __repr__(self):
+        return str(self.id)
 
 
 def dist_array(cities):
     """
 calculates distances between cities
 maybe np.linalg.norm
-    :param cities: list of cities
+    :param cities: list of cities objects
     :return dist_array: n x n array, with dist_array[i][j] = distance between ith and jth city
     """
     n = len(cities)
@@ -43,30 +46,30 @@ swaps two cities in configuration
     return new_config
 
 
-def change_config1(config):
-    """
-randomly changes configuration: e.g.: a--b--c--d--e--f ==> a--e--d--c--b--f
-    arg: old config
-    return: new config
-    """
-
-    return config
-
-
-def change_config2(config):
-    """
-randomly inserts a part of the config somewhere else, e.g. a--b--c--d--e--f ==> a--d--e--b--c--f
-    :param config: old config
-    :return: new config
-    """
-    return config
+# def change_config1(config):
+#     """
+# randomly changes configuration: e.g.: a--b--c--d--e--f ==> a--e--d--c--b--f
+#     arg: old config
+#     return: new config
+#     """
+#
+#     return config
+#
+#
+# def change_config2(config):
+#     """
+# randomly inserts a part of the config somewhere else, e.g. a--b--c--d--e--f ==> a--d--e--b--c--f
+#     :param config: old config
+#     :return: new config
+#     """
+#     return config
 
 
 def cost(config, dist):
     """
 calculates cost resp. distance for the configuration
     """
-    cost = sum([dist[i][(i + 1) % len(config)] for i in config])
+    cost = np.sum([dist[config[i]][config[(i + 1) % len(config)]] for i in range(len(config))])
     return cost
 
 
@@ -159,46 +162,124 @@ def run(config, t_k, dist, beta):
             config_accept = True
 
     return new_config, t_k1
-#3)repeat step 1, 2, keeping track of best solution until stoping cond
-
-# Animation------------------
-n = 100
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 1), ylim=(0, 1))
-line, = ax.plot([], [], animated=True, lw=1)
-points = np.random.rand(n, 2)
-ax.scatter(points[:, 0], points[:, 1], color='orange')
 
 
-def init():
-    line.set_data([], [])
-    return line,
+# TestSquare-----------------------------------
+# cities1 = [Cities([0,0]), Cities([0,1]), Cities([1,1]), Cities([1,0])]
+# dist1 = dist_array(cities1)
+# cost1 = cost([0,2,3,1],dist1)
+# print('cost1=',cost1)
+# print('dist1=',dist1)
+# -------------------------------------------------------------
+
+cities = random_cities(100)
+cities_dict = {}
+for city in cities:
+    cities_dict[city.id] = city
+
+x_cities = [c.position[0] for c in cities]
+y_cities = [c.position[1] for c in cities]
+
+dist = dist_array(cities)
+t_0 = temp_0(dist)
+list_id = [city.id for city in cities]
+start_config = np.random.permutation(list_id)
+beta = 0.9995
 
 
-def animate(i):
-    """"""
-    config = np.arange(n)
-    np.random.shuffle(config)
-    """"""
+accepted_configs = [start_config]
+best_config = start_config
+best_cost = cost(start_config, dist)
+t_min = 1e-6
+t = t_0
+config = start_config
 
-    xdata = points[config, 0]
-    ydata = points[config, 1]
-    line.set_data(xdata, ydata)
-    return line,
+counter = 0
+while t > t_min:
+    counter += 1
+    config, t = run(config, t, dist, beta)
+    accepted_configs.append(config)
 
+    if cost(config, dist) < cost(best_config, dist):
+        best_config = config
+        best_cost = cost(config, dist)
 
-# ax.scatter(xdata, ydata, s=200, color='black', zorder=1)
-# ax.scatter(xdata, ydata, s=200, color='orange', zorder=1)
-# ax.scatter(xdata, ydata, s=2, color='red', zorder=1)
+    if counter % 50000 == 0:
+        if accepted_configs[-1].all() == best_config.all():
+            break
+        counter = 0
 
+new_config, tk1 = run(start_config, t_0, dist, beta)
+plt.figure()
+fig, ax = plt.subplots(1, 3)
+for i in range(3):
+    ax[i].scatter(x_cities, y_cities)
 
-ani = animation.FuncAnimation(fig, animate, np.arange(0, 20), blit=True, interval=20,
-                              repeat=False, init_func=init)
+x_start_config = [cities_dict[id].position[0] for id in start_config]
+x_start_config.append(x_start_config[0])
+y_start_config = [cities_dict[id].position[1] for id in start_config]
+y_start_config.append(y_start_config[0])
+
+x_new_config = [cities_dict[id].position[0] for id in new_config]
+x_new_config.append(x_new_config[0])
+y_new_config = [cities_dict[id].position[1] for id in new_config]
+y_new_config.append(y_new_config[0])
+
+x_best_config = [cities_dict[id].position[0] for id in best_config]
+x_best_config.append(x_best_config[0])
+y_best_config = [cities_dict[id].position[1] for id in best_config]
+y_best_config.append(y_best_config[0])
+
+ax[0].plot(x_start_config, y_start_config)
+ax[1].plot(x_new_config, y_new_config)
+ax[2].plot(x_best_config, y_best_config)
+print(len(accepted_configs))
 plt.show()
-# Quinten--------------
+#
+# #3)repeat step 1, 2, keeping track of best solution until stoping cond
+#
+# # Animation------------------
+# n = 100
+# fig = plt.figure()
+# ax = plt.axes(xlim=(0, 1), ylim=(0, 1))
+# line, = ax.plot([], [], animated=True, lw=1)
+# points = np.random.rand(n, 2)
+# ax.scatter(points[:, 0], points[:, 1], color='orange')
+#
+#
+# def init():
+#     line.set_data([], [])
+#     return line,
+#
+#
+# def animate(i):
+#     """"""
+#     config = np.arange(n)
+#     np.random.shuffle(config)
+#     """"""
+#
+#     xdata = points[config, 0]
+#     ydata = points[config, 1]
+#     line.set_data(xdata, ydata)
+#     return line,
+#
+#
+# # ax.scatter(xdata, ydata, s=200, color='black', zorder=1)
+# # ax.scatter(xdata, ydata, s=200, color='orange', zorder=1)
+# # ax.scatter(xdata, ydata, s=2, color='red', zorder=1)
+#
+#
+# ani = animation.FuncAnimation(fig, animate, np.arange(0, 20), blit=True, interval=20,
+#                               repeat=False, init_func=init)
+# plt.show()
+# # Quinten--------------
+#
+#
+# if __name__ == '__main__':
+#     points, config = initial_condition(random_cities())
 
-
-if __name__ == '__main__':
-    points, config = initial_condition(random_cities())
-
-    
+# command = "{ffmpeg} -r {fps} -i {tmp_dir}/{qty}_image_%05d.png -vcodec mpeg4 -q:v 1 -y {out}".format(ffmpeg='C:\\FFmpeg',fps=10,tmp_dir=tmp_dir,qty=kwargs['qty'],out=outname)
+# subprocess.call(command,shell=True)
+#
+# fps feeds per second
+# tmp_dir = ordner mit bilder
